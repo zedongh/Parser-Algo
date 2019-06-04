c65d81f259941d72329f27605dfdc5d7336a7276
zedongh <248348907@qq.com>
2019-06-04 21:23:23 +0800

    change NFA definition

--- src/Automata/NFA.hs
+++ src/Automata/NFA.hs
@@ -1,14 +1,15 @@
 module Automata.NFA where 
 
-import qualified Data.Set as Set
-
 -- NFA representation as transition graph
+-- for simplicity, only one start State and one final state, 
+-- even though every state connect to final state with epsilon also is final state
 data NFA a = 
     NFA { edges :: [(Int, Int, a)]
         , startState  :: Int
-        , finalStates :: Int
+        , finalState :: Int
+        , charset :: [a]
         }
     deriving (Show, Eq)
 
 singleton :: Int -> Int -> a -> NFA a
-singleton i j a = NFA [(i, j, a)] i j
+singleton i j a = NFA [(i, j, a)] i j [a]
--- src/RE/RE.hs
+++ src/RE/RE.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 module RE.RE where 
 
-import qualified Data.Set as Set
+import Data.List (nub)
 import Automata.NFA
 import Control.Monad.State
 
@@ -13,7 +13,7 @@ data RE a = Or (RE a) (RE a)            -- r1 | r2
           deriving (Show, Functor)
 
 -- Using Nothing for epsilon trans
-re2nfa :: RE (Maybe a) -> NFA (Maybe a)
+re2nfa :: Eq a => RE (Maybe a) -> NFA (Maybe a)
 re2nfa re = evalState (re2nfas re) 0
 
 pick :: State Int Int
@@ -22,27 +22,28 @@ pick = do
     modify (+1)
     return i
 
-re2nfas :: RE (Maybe a) -> State Int (NFA (Maybe a))
+re2nfas :: Eq a => RE (Maybe a) -> State Int (NFA (Maybe a))
 re2nfas (Or r1 r2) = do     
     i <- pick
-    (NFA edges1 s1 f1) <- re2nfas r1
-    (NFA edges2 s2 f2) <- re2nfas r2
+    (NFA edges1 s1 f1 charset1) <- re2nfas r1
+    (NFA edges2 s2 f2 charset2) <- re2nfas r2
     j <- pick
     let edges = [(i, s1, Nothing), (i, s2, Nothing), (f1, j, Nothing), (f2, j, Nothing)] ++ edges1 ++ edges2
-    return $ NFA edges i j
+        charset = nub $ charset1 ++ charset2
+    return $ NFA edges i j charset
 re2nfas (Concat r1 r2) = do 
-    NFA edges1 s1 f1 <- re2nfas r1
+    NFA edges1 s1 f1 charset1 <- re2nfas r1
     modify (\x -> x - 1)
-    NFA edges2 s2 f2 <- re2nfas r2
+    NFA edges2 s2 f2 charset2 <- re2nfas r2
     if f1 /= s2 
         then error "should be impossible case, concat r1 final state should be r2 start state"
-        else return $ NFA (edges1 ++ edges2) s1 f2
+        else return $ NFA (edges1 ++ edges2) s1 f2 (nub $ charset1 ++ charset2)
 re2nfas (Star r) = do 
     i <- pick
-    (NFA edges s f) <- re2nfas r
+    (NFA edges s f charset) <- re2nfas r
     j <- pick
     let nedges = (i, j, Nothing):(i, s, Nothing):(f, s, Nothing):(f, j, Nothing):edges
-    return $ NFA nedges i j
+    return $ NFA nedges i j charset
 --              a
 -- start -> i -----> [j]  final state
 re2nfas (Single a) = do
